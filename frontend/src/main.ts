5c34198a89e23c3f22fe7c8cec15f18fe9b462ae
Hellyna NG <me@hellyna.com>
2019-11-14 18:53:21 +0800

    Initial commmit.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,10 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+Vue.config.productionTip = false
 
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+new Vue({
+  store,
+  render: h => h(App)
+}).$mount('#app')
